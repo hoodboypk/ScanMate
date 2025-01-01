fde5dc1b6b0183da560eae33370f0f8078c50b16
Pratik Kumar <pratik.k2004@gmail.com>
2025-01-01 18:30:07 +0530

    Update SwipeablePortfolio.cs

--- Assets/Scripts/SwipeablePortfolio.cs
+++ Assets/Scripts/SwipeablePortfolio.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SwipeablePortfolio : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public RectTransform portfolioContainer; // The container holding all images
+    public float swipeThreshold = 50f;       // Minimum swipe distance to register a swipe
+    public float lerpSpeed = 10f;            // Speed of smooth transition
+
+    private Vector2 touchStartPos;          // Start position of the touch
+    private Vector2 touchEndPos;            // End position of the touch
+    private int currentImageIndex = 0;      // Current image index
+    private int totalImages;                // Total number of images
+    private float imageWidth;               // Width of each image
+
+    private void Start()
     {
-        
+        // Calculate the total number of images and their width
+        totalImages = portfolioContainer.childCount;
+        imageWidth = portfolioContainer.rect.width / totalImages;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    touchStartPos = touch.position;
+                    break;
+
+                case TouchPhase.Ended:
+                    touchEndPos = touch.position;
+                    HandleSwipe();
+                    break;
+            }
+        }
+
+        // Smoothly move the container to the target position
+        Vector2 targetPos = new Vector2(-currentImageIndex * imageWidth, portfolioContainer.anchoredPosition.y);
+        portfolioContainer.anchoredPosition = Vector2.Lerp(portfolioContainer.anchoredPosition, targetPos, Time.deltaTime * lerpSpeed);
+    }
+
+    private void HandleSwipe()
+    {
+        float swipeDistance = touchEndPos.x - touchStartPos.x;
+
+        if (Mathf.Abs(swipeDistance) > swipeThreshold)
+        {
+            if (swipeDistance > 0 && currentImageIndex > 0)
+            {
+                // Swipe left for previous image
+                currentImageIndex--;
+            }
+            else if (swipeDistance < 0 && currentImageIndex < totalImages - 1)
+            {
+                // Swipe right for next image
+                currentImageIndex++;
+            }
+        }
     }
 }
